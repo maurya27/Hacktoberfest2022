90ba22cb905a59f386642bc8ea98aef37082b2b8
Gaya39877 <gayathrirasangikahw@gmail.com>
2022-10-24 10:31:45 +0530

    new comment

--- Add Code Here/PYTHON/Sorting/insertionSort.py
+++ Add Code Here/PYTHON/Sorting/insertionSort.py
@@ -8,3 +8,33 @@ for i in range(1, l):
         j -= 1
 
 print(array)
+
+"""
+# Python program for implementation of Insertion Sort
+
+# Function to do insertion sort
+def insertionSort(arr):
+
+	for i in range(1, len(arr)):
+
+		key = arr[i]
+
+		
+		j = i-1
+		while j >=0 and key < arr[j] :
+				arr[j+1] = arr[j]
+				j -= 1
+		arr[j+1] = key
+
+
+#sorting the array [10,34,24,28,31] using insertionSort
+arr = [10,34,24,28,31]
+insertionSort(arr)
+lst = [] 
+print("Sorted array is : ")
+for i in range(len(arr)):
+	lst.append(arr[i])	 
+print(lst)
+
+
+"""
